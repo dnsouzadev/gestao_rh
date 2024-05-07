e7071876c2188421cb5767136ceef7b0f6616aab
Daniel Souza <workdndsza@gmail.com>
2024-05-07 19:55:23 -0300

    iniciando o crud da empresa

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-# Create your views here.
+from apps.funcionarios.models import Funcionario
 
 
 @login_required
 def home(request):
-    return render(request, 'core/index.html')
+    data = {'usuario': request.user}
+    return render(request, 'core/index.html', data)
