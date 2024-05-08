c5c791e166cede04a2b8e677041dacbb7068869e
Daniel Souza <workdndsza@gmail.com>
2024-05-08 20:10:43 -0300

    update do funcionario

--- apps/funcionarios/views.py
+++ apps/funcionarios/views.py
@@ -1,9 +1,20 @@
-from django.views.generic import ListView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, UpdateView
 from .models import Funcionario
 
 
 class FuncionariosList(ListView):
     model = Funcionario
+
+    def get_queryset(self):
+        empresa_logada = self.request.user.funcionario.empresa
+        return Funcionario.objects.filter(empresa=empresa_logada)
+
+
+class FuncionarioEdit(UpdateView):
+    model = Funcionario
+    fields = ['nome', 'departamentos']
+    success_url = reverse_lazy('list_funcionarios')
 
     def get_queryset(self):
         empresa_logada = self.request.user.funcionario.empresa
