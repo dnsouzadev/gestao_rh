e279392bb874c7c078d5ceb9f87b7c650cdaa739
Daniel Souza <workdndsza@gmail.com>
2024-05-08 20:44:56 -0300

    create do crud de funcionarios

--- apps/funcionarios/urls.py
+++ apps/funcionarios/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
-from .views import FuncionariosList, FuncionarioEdit, FuncionarioDelete
+from .views import FuncionariosList, FuncionarioEdit, FuncionarioDelete, FuncionarioCreate
 
 urlpatterns = [
     path('', FuncionariosList.as_view(), name='list_funcionarios'),
     path('editar/<int:pk>', FuncionarioEdit.as_view(), name='edit_funcionarios'),
     path('delete/<int:pk>', FuncionarioDelete.as_view(), name='delete_funcionarios'),
+    path('criar', FuncionarioCreate.as_view(), name='create_funcionarios'),
 ]
