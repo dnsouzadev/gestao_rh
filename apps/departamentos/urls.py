552418add5de0214f828306a3b99ec9c8954c349
Daniel Souza <workdndsza@gmail.com>
2024-05-09 19:06:46 -0300

    terminando o crud do departamento

--- apps/departamentos/urls.py
+++ apps/departamentos/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path, include
-from .views import DepartamentosList
+from .views import DepartamentosList, DepartamentoCreate, DepartamentoUpdate, DepartamentoDelete
 
 urlpatterns = [
-    path('', DepartamentosList.as_view(), name='list_departamentos'),
-    path('create/', DepartamentoCreate.as_view(), name='create_departamento'),
+    path('list', DepartamentosList.as_view(), name='list_departamento'),
+    path('novo/', DepartamentoCreate.as_view(), name='create_departamento'),
     path('delete/<int:pk>/', DepartamentoDelete.as_view(), name='delete_departamento'),
-    path('update/<int:pk>/', DepartamentoEdit.as_view(), name='update_departamento'),
+    path('update/<int:pk>/', DepartamentoUpdate.as_view(), name='update_departamento'),
 
 ]
--- apps/departamentos/views.py
+++ apps/departamentos/views.py
@@ -1,15 +1,14 @@
 from django.shortcuts import render
-from django.views.generic import ListView, CreateView, DeleteView
-
-from apps.departamentos.models import Departamento
+from django.views.generic import ListView, CreateView, DeleteView, UpdateView
+from .models import Departamento
 
 
 class DepartamentosList(ListView):
     model = Departamento
 
-    # def get_queryset(self):
-    #     empresa_logada = self.request.user.funcionario.empresa
-    #     return Departamento.objects.filter(empresa=empresa_logada)
+    def get_queryset(self):
+        empresa_logada = self.request.user.funcionario.empresa
+        return Departamento.objects.filter(empresa=empresa_logada)
 
 
 class DepartamentoCreate(CreateView):
@@ -17,15 +16,28 @@ class DepartamentoCreate(CreateView):
     fields = ['nome']
     success_url = '/departamentos/list'
 
-    # def get_queryset(self):
-    #     empresa_logada = self.request.user.funcionario.empresa
-    #     return Departamento.objects.filter(empresa=empresa_logada)
+    def form_valid(self, form):
+        departamento = form.save(commit=False)
+        empresa_logada = self.request.user.funcionario.empresa
+        departamento.empresa = empresa_logada
+        departamento.save()
+        return super(DepartamentoCreate, self).form_valid(form)
+
+
+class DepartamentoUpdate(UpdateView):
+    model = Departamento
+    fields = ['nome']
+    success_url = '/departamentos/list'
+
+    def get_queryset(self):
+        empresa_logada = self.request.user.funcionario.empresa
+        return Departamento.objects.filter(empresa=empresa_logada)
 
 
 class DepartamentoDelete(DeleteView):
     model = Departamento
     success_url = '/departamentos/list'
 
-    # def get_queryset(self):
-    #     empresa_logada = self.request.user.funcionario.empresa
-    #     return Departamento.objects.filter(empresa=empresa_logada)
+    def get_queryset(self):
+        empresa_logada = self.request.user.funcionario.empresa
+        return Departamento.objects.filter(empresa=empresa_logada)
