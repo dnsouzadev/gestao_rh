36c2bf3a232a1db34af587952e50bd38fc2964d7
Daniel Souza <workdndsza@gmail.com>
2024-05-13 20:38:58 -0300

    termianndo o crud dos registros de hora extra

--- apps/registro_hora_extra/views.py
+++ apps/registro_hora_extra/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, UpdateView, CreateView, DeleteView
+
 from apps.registro_hora_extra.models import RegistroHoraExtra
 
 
@@ -9,3 +11,26 @@ class HoraExtraList(ListView):
     def get_queryset(self):
         empresa_logada = self.request.user.funcionario.empresa
         return RegistroHoraExtra.objects.filter(funcionario__empresa=empresa_logada)
+
+
+class HoraExtraEdit(UpdateView):
+    model = RegistroHoraExtra
+    fields = ['motivo', 'funcionario', 'horas']
+
+
+class HoraExtraCreate(CreateView):
+    model = RegistroHoraExtra
+    fields = ['motivo', 'funcionario', 'horas']
+
+    def form_valid(self, form):
+        hora_extra = form.save(commit=False)
+        hora_extra.registrado_por = self.request.user
+        hora_extra.save()
+        return super(HoraExtraCreate, self).form_valid(form)
+
+
+class HoraExtraDelete(DeleteView):
+    model = RegistroHoraExtra
+
+    def get_success_url(self):
+        return reverse_lazy('list_hora_extra')
