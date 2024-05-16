66d640c6fe459ee2425a6d46bc1a7ad3f3664154
Daniel Souza <workdndsza@gmail.com>
2024-05-16 19:42:46 -0300

    adicionando static_root

--- gestao_rh/settings.py
+++ gestao_rh/settings.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -109,6 +109,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
