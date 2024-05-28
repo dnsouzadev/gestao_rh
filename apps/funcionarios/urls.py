2a9b8672d7b6f3f4d865af02d267c9eadeba53cd
Daniel Souza <workdndsza@gmail.com>
2024-05-28 19:05:44 -0300

    gerando relatorio com reportlab com pdf

--- apps/funcionarios/urls.py
+++ apps/funcionarios/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
-from .views import FuncionariosList, FuncionarioEdit, FuncionarioDelete, FuncionarioCreate
+from .views import FuncionariosList, FuncionarioEdit, FuncionarioDelete, FuncionarioCreate, test_view
 
 urlpatterns = [
     path('', FuncionariosList.as_view(), name='list_funcionarios'),
     path('editar/<int:pk>', FuncionarioEdit.as_view(), name='edit_funcionarios'),
     path('delete/<int:pk>', FuncionarioDelete.as_view(), name='delete_funcionarios'),
     path('criar', FuncionarioCreate.as_view(), name='create_funcionarios'),
+    path('pdf-view/', test_view, name='pdf_funcionarios'),
 ]
--- apps/funcionarios/views.py
+++ apps/funcionarios/views.py
@@ -1,6 +1,11 @@
+from io import BytesIO
+
 from django.contrib.auth.models import User
+from django.http import HttpResponse
 from django.urls import reverse_lazy
 from django.views.generic import ListView, UpdateView, DeleteView, CreateView
+from reportlab.pdfgen import canvas
+
 from .models import Funcionario
 
 
@@ -43,3 +48,32 @@ class FuncionarioCreate(CreateView):
             username=funcionario.nome.split(' ')[0] + funcionario.nome.split(' ')[1])
         funcionario.save()
         return super(FuncionarioCreate, self).form_valid(form)
+
+
+def test_view(request):
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'attachment; filename="mypdf.pdf"'
+
+    buffer = BytesIO()
+    p = canvas.Canvas(buffer)
+
+    p.drawString(150, 810, 'Relatório de Funcionários')
+
+    funcionarios = Funcionario.objects.all()
+
+    str = 'Nome: %s | Hora Extra: %s'
+
+    y = 750
+    for funcionario in funcionarios:
+        print(funcionario.nome, funcionario.total_horas_extra)
+        p.drawString(10, y, str % (funcionario.nome, funcionario.total_horas_extra))
+        y -= 50
+
+    p.showPage()
+    p.save()
+
+    pdf = buffer.getvalue()
+    buffer.close()
+    response.write(pdf)
+
+    return response
